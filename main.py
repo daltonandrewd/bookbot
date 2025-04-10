912a1430c282a37534b2cba83234042c563312d0
daltonandrewd <daltonandrewd@gmail.com>
2025-04-09 22:36:13 -0500

    Added book reader to Main, and added word counter in stats.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+    
+def get_book_text(book):
+    with open(book) as f:
+        return f.read()
+
+from stats import get_num_words
+
+
+main()
