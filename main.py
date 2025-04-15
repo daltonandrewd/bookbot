86c5f8cad5d55476adb3c27ef83021b280041324
daltonandrewd <daltonandrewd@gmail.com>
2025-04-14 21:42:16 -0500

    Added the letter counting part to both stats and main.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from stats import get_num_words, get_num_letters
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
     print(f"{num_words} words found in the document")
+    print(num_letters)
     
 def get_book_text(book):
     with open(book) as f:
         return f.read()
-
-from stats import get_num_words
-
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
         words = text.split()
         return len(words)
+
+def get_num_letters(text):
+        lower_words = [letter.lower() for letter in text]
+        letter_counts = {}
+        for letter in lower_words:
+                if letter not in letter_counts:
+                        letter_counts[letter] = 1
+                else:
+                        letter_counts[letter] += 1
+        return letter_counts
